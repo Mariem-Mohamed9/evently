91055b9e15e1487d54378cfa63f1b0a2351ca9f0
Mariem-Mohamed9 <mariammohamedsabet@gmail.com>
2025-07-21 02:01:52 +0300

    NativeSplashScreen

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(EventlyApp());
+}
+
+class EventlyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
